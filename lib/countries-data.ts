f6bf749bb23ceeb3009482120d44acb2792cb560
James Onashile <jonashile@gmail.com>
2025-05-22 21:54:28 +0100

    feat: add dummy data with ISO Alpha-2 codes amd phases

---
+++ lib/countries-data.ts
@@ -0,0 +1,9 @@
+export const countries = [
+    { code: "US", dividendPhase: "Post-Dividend" },
+    { code: "IN", dividendPhase: "Pre-Dividend" },
+    { code: "CN", dividendPhase: "Post-Dividend" },
+    { code: "DE", dividendPhase: "Post-Dividend" },
+    { code: "JP", dividendPhase: "Post-Dividend" },
+  ];
+
+  
